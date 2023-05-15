d2ed3d20f138fa6da3318e11245e6b02485455ab
gustavoCorreiaGonzalez <gustavo.correia.gonzalez@gmail.com>
2023-05-15 19:42:12 -0300

    feat: :sparkles: Utilizando dataclasses

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,11 +1,15 @@
 from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
 
 
+def current_date():
+    return datetime.now()
+
+
+@dataclass()
 class Category:
-    def __init__(
-        self, name: str, description: str, is_active: bool, created_at: datetime
-    ) -> None:
-        self.name = name
-        self.description = description
-        self.is_active = is_active
-        self.created_at = created_at
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = field(default=True)
+    created_at: Optional[datetime] = field(default_factory=current_date)
